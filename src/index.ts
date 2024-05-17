b6bf5b9103ed5fe04bef4500134e70ebc3f6f214
Braylon Mooney <99223145+trixie909@users.noreply.github.com>
2024-05-17 16:30:11 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,17 +25,7 @@ process.on("uncaughtException", (err: string) => {
 	console.error(err);
 });
 
-process.stdout.write(`
-\x1b[38;2;143;110;250m████████╗██╗ ██████╗██╗  ██╗███████╗████████╗    ██████╗  ██████╗ ████████╗
-\x1b[38;2;157;101;254m╚══██╔══╝██║██╔════╝██║ ██╔╝██╔════╝╚══██╔══╝    ██╔══██╗██╔═══██╗╚══██╔══╝
-\x1b[38;2;172;90;255m   ██║   ██║██║     █████╔╝ █████╗     ██║       ██████╔╝██║   ██║   ██║   
-\x1b[38;2;188;76;255m   ██║   ██║██║     ██╔═██╗ ██╔══╝     ██║       ██╔══██╗██║   ██║   ██║   
-\x1b[38;2;205;54;255m   ██║   ██║╚██████╗██║  ██╗███████╗   ██║       ██████╔╝╚██████╔╝   ██║   
-\x1b[38;2;222;0;255m   ╚═╝   ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝       ╚═════╝  ╚═════╝    ╚═╝\x1b[0m
-                 https://github.com/Sayrix/ticket-bot
-
-Connecting to Discord...
-`);
+process.stdout.write(`connecting to PROBE.`);
 
 // Update Detector
 fetch("https://api.github.com/repos/Sayrix/Ticket-Bot/tags").then((res) => {
